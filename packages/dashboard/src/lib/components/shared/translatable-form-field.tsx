a6fd24837dd40c91f5832f1521e969e9cb2022c5
Gabriel Bui <a.buiquang@elevantiq.com>
2026-03-16 16:24:04 +0100

    fix(dashboard): Forward all ControllerProps in FormFieldWrapper
    Fixes #4511

--- packages/dashboard/src/lib/components/shared/translatable-form-field.tsx
+++ packages/dashboard/src/lib/components/shared/translatable-form-field.tsx
@@ -35,17 +35,17 @@ export type TranslatableFormFieldProps<TFieldValues extends TranslatableEntity |
     name: TFieldValues extends TranslatableEntity
         ? keyof Omit<NonNullable<TFieldValues['translations']>[number], 'languageCode'>
         : TFieldValues extends TranslatableEntity[]
-            ? keyof Omit<NonNullable<TFieldValues[number]['translations']>[number], 'languageCode'>
-            : never;
+          ? keyof Omit<NonNullable<TFieldValues[number]['translations']>[number], 'languageCode'>
+          : never;
 };
 
 export const TranslatableFormField = <
     TFieldValues extends TranslatableEntity | TranslatableEntity[] = TranslatableEntity,
 >({
-      name,
-      label,
-      ...props
-  }: TranslatableFormFieldProps<TFieldValues>) => {
+    name,
+    label,
+    ...props
+}: TranslatableFormFieldProps<TFieldValues>) => {
     const { formatLanguageName } = useLocalFormat();
     const { contentLanguage } = useUserSettings().settings;
     const formValues = props.control?._formValues;
@@ -112,19 +112,17 @@ export type TranslatableFormFieldWrapperProps<
 export const TranslatableFormFieldWrapper = <
     TFieldValues extends TranslatableEntity | TranslatableEntity[] = TranslatableEntity,
 >({
-      name,
-      label,
-      description,
-      render,
-      renderFormControl,
-      ...props
-  }: TranslatableFormFieldWrapperProps<TFieldValues>) => {
+    label,
+    description,
+    renderFormControl,
+    ...controllerProps
+}: TranslatableFormFieldWrapperProps<TFieldValues>) => {
+    const { name, render } = controllerProps;
     return (
         <LocationWrapper identifier={name as string}>
             <TranslatableFormField
+                {...controllerProps}
                 label={label}
-                control={props.control}
-                name={name}
                 render={renderArgs => (
                     <FormItem>
                         {label && <FormLabel>{label}</FormLabel>}
